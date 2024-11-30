3373b86f9070250b5260522f024c6553c75bb3d2
Madina <madinanavrazova@gmail.com>
2024-12-01 02:22:14 +0300

    2commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.backgroundImage = "url('Img/фон2.png')";
+    document.body.style.backgroundPosition="bottom center";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
